00930b6cf13a8c6f9dd1f841baf05c78a07043a8
ruby45dev <60508600+ruby45dev@users.noreply.github.com>
2023-07-27 12:44:08 +0300

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,36 @@
+puts "Привіт! Занотуй свої думки: "
+
+# current_puth = File.dirname(__FILE__) #відносний 
+# current_puth = Dir.pwd #абсолютний
+current_puth = __dir__ #абсолютний
+
+line = nil
+all_lines = []
+
+while line != 'end' do
+    line = STDIN.gets.chomp
+    all_lines << line
+end
+
+time = Time.now
+
+file_name = time.strftime('%Y-%m-%d') # Time.now.strftime('%Y-%m-%d_%H:%M')
+time_string = time.strftime('%H:%M')
+
+message_separator = "******************************************"
+
+file = File.new(current_puth + "/" + file_name + ".txt", 'a')
+
+file.print("\n\r" + time_string + "\n\r")
+
+all_lines.pop
+
+all_lines.each do |item|
+    file.puts(item)
+end                         
+
+file.puts(message_separator)
+
+file.close
+
+puts "Записано у файл #{file_name}.txt"
